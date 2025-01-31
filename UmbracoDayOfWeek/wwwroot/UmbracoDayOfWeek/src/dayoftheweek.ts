efc63057407328b07d48cdff7a15d3e8b0e16af5
eric <efr@umbraco.com>
2025-01-31 14:35:53 +0100

    resolve issue with dropdown select

--- UmbracoDayOfWeek/wwwroot/UmbracoDayOfWeek/src/dayoftheweek.ts
+++ UmbracoDayOfWeek/wwwroot/UmbracoDayOfWeek/src/dayoftheweek.ts
@@ -1,81 +1,75 @@
-//@ts-ignore
+// @ts-ignore
 import { LitElement, html, customElement, property, state } from "@umbraco-cms/backoffice/external/lit";
-//@ts-ignore
+// @ts-ignore
 import { UmbElementMixin } from "@umbraco-cms/backoffice/element-api";
-//@ts-ignore
+// @ts-ignore
 import { UMB_AUTH_CONTEXT, UmbAuthContext } from '@umbraco-cms/backoffice/auth';
-//@ts-ignore
+// @ts-ignore
 import { UmbPropertyValueChangeEvent } from '@umbraco-cms/backoffice/property-editor';
-//@ts-ignore
+// @ts-ignore
 import { type UmbPropertyEditorConfigCollection, UmbPropertyEditorUiElement } from "@umbraco-cms/backoffice/property-editor";
-//@ts-ignore
+// @ts-ignore
 import type { UUISelectEvent } from '@umbraco-cms/backoffice/external/uui';
 
 @customElement('my-dayoftheweek')
 export class MyDayOfTheWeekElement extends UmbElementMixin(LitElement) implements UmbPropertyEditorUiElement {
-  #selection: string = "";
+  #selection: number = -1;
   
-  public get value(): string | undefined {
+  public get value(): number{
     return this.#selection;
   };
-
+  
+  @property()
+  public displayList: Array<any> = [];
+  
   @property({ type: Number })
   public defaultStartOfTheWeek: number = 1; // Monday by default
   
-  @property({ type: String })
-  public set value(value: string | null | undefined) {
-    this.#selection = value || "";
+  @property({ type: Number })
+  public set value(value: number) {
+    this.#selection = value;
   }
   
   @property()
   public myAuthToken: Promise<string> | undefined;
   
-  @property({ type: Boolean, reflect: true })
-  readonly = false;
-  
   @property({ attribute: false})
   public set config(config: UmbPropertyEditorConfigCollection | undefined) {
-    
     let defaultStartDayOfWeekConfigValue = config.getValueByAlias("startOfWeek");
     this._startOfWeek = defaultStartDayOfWeekConfigValue ? defaultStartDayOfWeekConfigValue : this.defaultStartOfTheWeek;
-
-    const items = defaultStartDayOfWeekConfigValue ? defaultStartDayOfWeekConfigValue : this.defaultStartOfTheWeek;
-    
-    if (Array.isArray(items) && items.length > 0) {
-      this._options =
-          typeof items[0] == 'string'
-              ? items.map((item) => ({ name: item, value: item, selected: this.#selection.includes(item) }))
-              : items.map((item) => ({
-                name: item.name,
-                value: item.value,
-                selected: this.#selection.includes(item.value),
-              }));
-    }
   }
-  
-  //@ts-ignore
+  // @ts-ignore  
   private _authorizationContext: UmbAuthContext;
   
   @state()
-  //@ts-ignore
+  // @ts-ignore
   private _startOfWeek: number;
   
-  
   @state()
-  //@ts-ignore
+  // @ts-ignore
   private _options: Array<Option> = [];
   
-  #onChange(event: UUISelectEvent){
-    const value = event.target.value;
-    this.#setValue(value);
-  }
-  #setValue(value: string | null | undefined) {
-    if (!value) return;
-    this.value = value;
-    //@ts-ignore
-    this.dispatchEvent(new UmbPropertyValueChangeEvent());
+  // @ts-ignore
+  #mapList(){
+    // @ts-ignore
+    this.displayList = this.list.map(item =>({
+      // @ts-ignore
+      name: this.localize.term(`DayOfTheWeek_d${item.id}`) || item.DefaultName,
+      value: item.id,
+      selected: this.value || this.value === 0 ? this.value == item.id : false
+    }));
+    // @ts-ignore
+    this.requestUpdate();
+    console.log(this.displayList)
   }
   
+  #onChange(event: UUISelectEvent){
+    this.value = +event.target.value;
+    // @ts-ignore
+    this.dispatchEvent(new UmbPropertyValueChangeEvent())
+    this.#mapList()
+
+  }
   constructor() {
     super();
     // @ts-ignore
@@ -87,44 +81,37 @@ export class MyDayOfTheWeekElement extends UmbElementMixin(LitElement) implement
   render() {
     return html`
       <umb-input-dropdown-list
-          .options=${this._options.map(option => ({ 
-            name: option.name, 
-            value: option.value, 
-            selected: option.selected }))}
-          @change=${this.#onChange}
-          ?readonly=${this.readonly}>
+          .options=${this.displayList}
+          @change=${this.#onChange}>
       </umb-input-dropdown-list>
     `
   }
-    async connectedCallback(changed: any) {
-        super.connectedCallback(changed);
-        await this.getDayOfTheWeek();
-    }
-  private async getDayOfTheWeek() {
-    try{
-      const promiseToken: string | undefined = await this.myAuthToken;
-      const headers = {
-        'Authorization': `Bearer ${promiseToken}`
-      };
-      
-      const response = await fetch(`/umbraco/management/api/v1/get-dropdown-value-list?startDayOfTheWeek=${this._startOfWeek}`, {headers}).then(response => response.json());
-      
-      this._options = response.map((item: any) => {
-        //@ts-ignore
-        let localizedTerm = this.localize.term(`DayOfTheWeek_d${item.id}`);
-        return{
-          name: localizedTerm !== `DayOfTheWeek_d${item.id}` ? localizedTerm : item.defaultName,
-          value: item.id,
-          selected: this.value ? this.value == item.id : false
-        }
+    async connectedCallback() {
+      super.connectedCallback();
+      const defaultStartDayOfWeekConfigValue = this._startOfWeek;
+      // @ts-ignore
+      this.defaultStartDayOfWeekValue = defaultStartDayOfWeekConfigValue >= 0 && defaultStartDayOfWeekConfigValue < 7 ? defaultStartDayOfWeekConfigValue : this.defaultStartDayOfWeekValue;
+      // @ts-ignore
+      this.consumeContext(UMB_AUTH_CONTEXT, (context) => {
+        // @ts-ignore
+        context.getLatestToken().then(promiseToken => {
+          try {
+            const headers = {
+              Authorization: `Bearer ${promiseToken}`
+            };
+            // @ts-ignore
+            fetch(`/umbraco/management/api/v1/get-dropdown-value-list?startDayOfTheWeek=${this.defaultStartDayOfWeekValue}`, {headers}).then(response => response.json())
+                .then(results => {
+                  // @ts-ignore
+                  this.list = results;
+                  this.#mapList()
+                })
+          } catch (e) {
+            console.error(e);
+          }
+        });
       });
-      
-      console.log(this._options);
-
-    } catch (e) {
-      console.error(e);
     }
-  }
 }
 
 declare global {
